62de94f058050bb95d6ca973f3a3c8ecac9ff96b
astro-jets <mphatsomtogolo30@gmail.com>
2023-11-16 22:37:34 +0200

    changed stories from CSR to SSR

--- app/components/Ads.jsx
+++ app/components/Ads.jsx
@@ -5,22 +5,22 @@ const Ads = () => {
                 <div className="hs-carousel-body absolute top-0 bottom-0 start-0 flex flex-nowrap transition-transform duration-700 opacity-0">
                     <div className="hs-carousel-slide">
                         <div className="h-full relative">
-                        <img src="/kfc.png" alt="" className="w-full h-full object-cover" />
+                        <img src="/kfc.jpg" alt="" className="w-11/12 h-11/12 object-cover" />
                         </div>
                     </div>
                     <div className="hs-carousel-slide">
                         <div className="h-full relative">
-                        <img src="/escom.jpg" alt="" className="w-full h-full object-cover" />
+                        <img src="/escom.jpg" alt="" className="w-11/12 h-11/12 object-cover" />
                         </div>
                     </div>
                     <div className="hs-carousel-slide">
                         <div className="h-full relative">
-                        <img src="/ad.png" alt="" className="w-full h-full object-cover" />
+                        <img src="/ad.png" alt="" className="w-11/12 h-11/12 object-cover" />
                         </div>
                     </div>
                     <div className="hs-carousel-slide">
                         <div className="h-full relative">
-                        <img src="/malemia.jpg" alt="" className="w-full h-full object-cover" />
+                        <img src="/adam.jpg" alt="" className="w-11/12 h-11/12 object-cover" />
                         </div>
                     </div>
                 </div>
