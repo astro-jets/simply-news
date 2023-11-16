a8bd5b9d11127041241f1a2c96ff12b64551352a
astro-jets <mphatsomtogolo30@gmail.com>
2023-11-16 22:42:58 +0200

    reverted the style of images in ads

--- app/components/Ads.jsx
+++ app/components/Ads.jsx
@@ -4,23 +4,23 @@ const Ads = () => {
             <div className="hs-carousel relative overflow-hidden w-full bg-blue-300 h-[350px] rounded-lg p-0">
                 <div className="hs-carousel-body absolute top-0 bottom-0 start-0 flex flex-nowrap transition-transform duration-700 opacity-0">
                     <div className="hs-carousel-slide">
-                        <div className="h-full relative">
-                        <img src="/kfc.jpg" alt="" className="w-full h-[85%] object-cover" />
+                        <div className="h-full relative p-2">
+                        <img src="/kfc.jpg" alt="" className="w-full h-full object-cover" />
                         </div>
                     </div>
                     <div className="hs-carousel-slide">
-                        <div className="h-full relative">
-                        <img src="/escom.jpg" alt="" className="w-full h-[85%] object-cover" />
+                        <div className="h-full relative p-2">
+                        <img src="/escom.jpg" alt="" className="w-full h-full object-cover" />
                         </div>
                     </div>
                     <div className="hs-carousel-slide">
-                        <div className="h-full relative">
-                        <img src="/ad.png" alt="" className="w-full h-[85%] object-cover" />
+                        <div className="h-full relative p-2">
+                        <img src="/ad.png" alt="" className="w-full h-full object-cover" />
                         </div>
                     </div>
                     <div className="hs-carousel-slide">
-                        <div className="h-full relative">
-                        <img src="/adam.jpg" alt="" className="w-full h-[85%] object-cover" />
+                        <div className="h-full relative p-2">
+                        <img src="/adam.jpg" alt="" className="w-full h-full object-cover" />
                         </div>
                     </div>
                 </div>
