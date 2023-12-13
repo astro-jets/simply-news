97981ad0d9d6a36a9986c4c0c8dc9dc7300255de
astro-jets <mphatsomtogolo30@gmail.com>
2023-12-13 13:05:49 +0200

    Removed database

--- app/api/blogs/new/route.js
+++ app/api/blogs/new/route.js
@@ -1,29 +1,29 @@
-import connectDB from "@/utils/db";
-import Blog from "@/models/blog";
-import { NextResponse } from "next/server";
+// import connectDB from "@/utils/db";
+// import Blog from "@/models/blog";
+// import { NextResponse } from "next/server";
 
-export const dynamic = 'force-dynamic';
+// export const dynamic = 'force-dynamic';
 
-export async function POST (req){
-    await connectDB();
-    const {title,story,category,image,imageType} = await req.json();
-    try{
-        const newBlog = await Blog.create({
-            title,story,category,image,imageType
-        });
+// export async function POST (req){
+//     await connectDB();
+//     const {title,story,category,image,imageType} = await req.json();
+//     try{
+//         const newBlog = await Blog.create({
+//             title,story,category,image,imageType
+//         });
 
-        if(newBlog){
-            return NextResponse.json({
-                status:"true",
-                message:"Blog created successfully"
-            });
-        }
-    }
-    catch(err){
-        console.log(`Failed to post Blog ${err}`);
-        return NextResponse.json({
-            status:"error",
-            message:"Something went wrong"
-        })
-    }
-}
+//         if(newBlog){
+//             return NextResponse.json({
+//                 status:"true",
+//                 message:"Blog created successfully"
+//             });
+//         }
+//     }
+//     catch(err){
+//         console.log(`Failed to post Blog ${err}`);
+//         return NextResponse.json({
+//             status:"error",
+//             message:"Something went wrong"
+//         })
+//     }
+// }
