940d6480ad4bac89de5d41e21d513a11f5f885d2
astro-jets <mphatsomtogolo30@gmail.com>
2023-12-13 10:44:39 +0200

    Changed DB connection

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://mphatso:kigSroUQMKRgJlmE@cluster0.kppm2on.mongodb.net/");
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
