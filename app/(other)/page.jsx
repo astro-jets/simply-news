ea2b70eab2c188cbe8f007315b927132364640d6
astro-jets <mphatsomtogolo30@gmail.com>
2023-12-14 09:09:34 +0200

    facing hard times

--- app/(other)/page.jsx
+++ app/(other)/page.jsx
@@ -1,37 +1,78 @@
-'use client'
-import { useEffect } from "react";
+// 'use client'
+// import { useEffect, useState } from "react";
+// import clientPromise from "@/lib/mongodb";
 
 // Components
-import Stories from './components/Stories';
-import AnnouncementBanner  from './components/AnnoucementBanner';
-import ContactForm from './components/ContactForm';
-import Slider from './components/Slider';
-import Ads from "./components/Ads";
-import Videos from "./components/Videos";
+// import Stories from './components/Stories';
+// import AnnouncementBanner  from './components/AnnoucementBanner';
+// import ContactForm from './components/ContactForm';
+// import Slider from './components/Slider';
+// import Ads from "./components/Ads";
+// import Videos from "./components/Videos";
 
-export default function Home() {
-  useEffect(() => {
-  const loadPreline = async () => {
-    await import('preline');
-    await import('@preline/carousel');
-  };
-  loadPreline();
-}, []);
+export default async function Home() {
+  // const [restaurants,setRestaurants] = useState([]);
+  // useEffect(() => {
+  //   const loadPreline = async () => {
+  //     await import('preline');
+  //     await import('@preline/carousel');
+  //   };
+  //   loadPreline();
+
+  //   (async()=>{
+  //     const results = await fetch('http://localhost:3000/api/list')
+  //     const resJson = await results.json();
+  //     setRestaurants(resJson);
+  //   })
+
+// }, []);
+
+  const rootLink = process.env.ROOT_LINK;
+  const response = await fetch(`${rootLink}/api/list`,{cache:'no-cache'});
+  const restaurants = await response.json();
 
   return (
-    <>
-      <div className="flex flex-col md:space-x-2 md:flex-row h-full m-0">
-        <div className="md:w-3/4 ">
-          <Slider/>
-        </div>
-        <div className="md:w-2/4 h-[70vh] hidden md:flex">
-          <Ads />
-        </div>
+      restaurants?
+      // <>
+      //   <div className="flex flex-col md:space-x-2 md:flex-row h-full m-0">
+      //     <div className="md:w-3/4 ">
+      //       <Slider/>
+      //     </div>
+      //     <div className="md:w-2/4 h-[70vh] hidden md:flex">
+      //       <Ads />
+      //     </div>
+      //   </div>
+      //   <Stories/>
+      //   <AnnouncementBanner />
+      //   <Videos/>
+      //   <ContactForm/>
+      // </>
+      <div className="grid">
+        {
+          restaurants.map((r)=>(
+            <div className="card" key={r._id}>
+              <div className="my-16">
+                <h2>{r.name}</h2>
+                <p className="text-white" >{r.cuisine}</p>
+              </div>
+            </div>
+          ))
+        }
       </div>
-      <Stories/>
-      <AnnouncementBanner />
-      <Videos/>
-      <ContactForm/>
-    </>
+      :null
   )
 }
+
+// export async function getServerSideProps(context){
+//   try{
+//     await clientPromise;
+//     return{
+//       props:{isConnected:true},
+//     }
+//   }catch(e){
+//     console.log(e);
+//     return{
+//       props:{isConnected:false},
+//     }
+//   }
+// }
---
+++ app/api/food/route.js
@@ -0,0 +1,31 @@
+import clientPromise from "@/lib/mongodb";
+import { NextResponse } from "next/server";
+
+
+export async function GET(req,res)
+{
+    try{
+        const mongoClient = await clientPromise; 
+        // Databse Name
+        const db = mongoClient.db("sample_restaurants");
+
+        // Table
+        const collection = db.collection("restaurants");
+
+        // Fetch the data
+        const results = await collection
+        .find({})
+        .project({
+            grades:0,
+            borough:0,
+            restaurant_id:0,
+        })
+        .limit(2)
+        .toArray();  
+        
+        // Return the result
+        return NextResponse.json(results)
+    }catch(e){
+        console.log("Failed to fetch Data => ",e);
+    }
+}
